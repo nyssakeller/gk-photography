0fe5a7c7e6dd13227eb24c2072a00d74f40f2b82
Nyssa Keller <nyssa1@gmail.com>
2018-06-11 18:24:27 -0600

    Add NavLinks to the header. Include <BrowserRouter> in the index.js

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { NavLink, Route } from 'react-router-dom';
 import './Header.css';
 
 class Header extends Component {
@@ -6,6 +7,11 @@ class Header extends Component {
     return (
       <div className='header'>
         <h1>Gwen Keller Photography</h1>
+        <div>
+          <NavLink to='/about' className='nav'>About</NavLink>
+          <NavLink to='/portraits' className='nav'>Portraits</NavLink>
+          <NavLink to='/contact' className='nav'>Contact</NavLink>
+        </div>
       </div>
     );
   }
