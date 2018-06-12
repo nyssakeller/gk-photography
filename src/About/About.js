b42fa329d38c86eaf0d33b061994b8133e01b3e7
Nyssa Keller <nyssa1@gmail.com>
2018-06-12 14:24:45 -0600

    Create About component

---
+++ src/About/About.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './About.css';
+
+class About extends Component {
+  render() {
+    return (
+      <div></div>
+    );
+  }
+};
+
+export default About;
